7c5a0878c9d85bedfe6a004b0df3f174c2f50f85
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-01-23 12:13:35 -0300

    fix: Corrige estrutura básica do ExceptionHandler da aplicação

--- services/credor-service/src/main/java/emprestimos/com/credor/exception/handler/ExceptionHandler.java
+++ services/credor-service/src/main/java/emprestimos/com/credor/exception/handler/ExceptionHandler.java
@@ -1,11 +1,9 @@
 package emprestimos.com.credor.exception.handler;
 
-import jakarta.annotation.Nonnull;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
@@ -17,14 +15,6 @@ import java.util.Map;
 @RestController
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @Nonnull
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
