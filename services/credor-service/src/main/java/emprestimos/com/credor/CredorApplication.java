2e85cb9af54efbb7ee0704fdcf9187adcade0197
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-01-23 11:13:20 -0300

    refac: Refatora nome da classe principal para CredorApplication

--- services/credor-service/src/main/java/emprestimos/com/credor/CredorApplication.java
+++ services/credor-service/src/main/java/emprestimos/com/credor/CredorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CredorServiceApplication {
+public class CredorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CredorServiceApplication.class, args);
+		SpringApplication.run(CredorApplication.class, args);
 	}
 
 }
