b8b65c17d4d93cf0c7a5cd1419ae135cdce6e3c8
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-01-24 12:03:33 -0300

    feat: Adiciona @ToString ao CredorEntity

--- services/credor-service/src/main/java/emprestimos/com/credor/models/entity/CredorEntity.java
+++ services/credor-service/src/main/java/emprestimos/com/credor/models/entity/CredorEntity.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 
 import java.math.BigDecimal;
@@ -22,6 +19,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "tb_credor")
